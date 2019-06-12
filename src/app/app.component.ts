5f2e06fb3cec57541f311ee1d834bbe19aa139f5
Stephen Trewick and sabrina mianne <stephen+sabrina@>
2019-06-12 15:04:22 -0700

    initialized a kombucha taproom for patrons and employees with displayed tap list and password form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  //Editable greeting
+  seasonalGreeting: string = 'Refresh this Summer with our quenching selection';
+  //List of kegs;
+  kegs: Keg[] = [
+    new Keg('Watermelon Mint', 'ABC', 5.00, 4.5, 0 ),
+    new Keg('Detox', 'HealthCo.', 6.00, 3.4, 0),
+    new Keg('Lemon Strawberry', 'Kombucha Inc.', 4.00, 2.1, 100)
+  ];
+ 
+  getStatus(currentTask) {
+    if (currentTask.amount === 0 )
+    {
+      currentTask.available = false;
+      return "No";
+    }
+    else
+    {
+      return "Yes";
+    }
+  }
+
+  isAuth: boolean = false;
+  password: string = "1234";
+  employeePassword: string;
+
+  onSubmit() {
+    return this.employeePassword;
+  }
+
+  CheckPassword() {
+    if(this.password===this.employeePassword)
+    {
+      this.isAuth = true;
+    }
+  }
+
+
+
+  selectedKeg = null;
+
 }
+
+
---
+++ src/app/models/keg.model.ts
@@ -0,0 +1,4 @@
+export class Keg {
+    constructor(public name: string, public brand: string, public price: number, public ABV: number, public amount: number, public available: boolean = true) {}
+
+}
